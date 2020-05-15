240d4df85c7be2cc431bd69077c6f82da21edbdc
Alex Sutton <alexsutton14@hotmail.com>
2020-05-15 16:36:09 +0100

    Form working

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,20 +1,11 @@
 class FlightsController < ApplicationController
     def index
         @params = params
-        @selected_start = 1
-        if (params[:start_airport_id].present?)
-        @selected_start = params[:start_airport_id]
-        @departing_flights = Airport.find(params[:start_airport_id]).departing_flights
-        @destination_airports_select = @departing_flights.map{ |f| [ f.end_airport.code, f.end_airport.id]}
-        end
-        if(@destination_airports_select == nil)
-            @destination_airports_select = ["-"]
-        end
-        @start_airports_select = Airport.all.map{ |a| [a.code, a.id]}
+        @airports_select = Airport.all.map{ |a| [a.code, a.code]}
     end
 
     private
     def flight_params(params)
-        params.permit(:start_airport_id, :end_airport_id)
+        params.permit(:start_code, :end_code)
     end
 end
