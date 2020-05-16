8dbf28028a596749b88357bf0d47dd92c5354008
Alex Sutton <alexsutton14@hotmail.com>
2020-05-16 15:39:34 +0100

    Created Booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_182847) do
+ActiveRecord::Schema.define(version: 2020_05_16_143641) do
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id"
+    t.integer "number_of_passengers"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
